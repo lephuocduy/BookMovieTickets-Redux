159469a33d0531e6870c5b9d7f95365aa9a2654c
lephuocduy <duylp.work@gmail.com>
2021-06-29 00:35:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapDatVePhim from './BaiTapDatVePhim/BaiTapDatVePhim';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapDatVePhim />
+    </div>
+  );
+}
+
+export default App;
